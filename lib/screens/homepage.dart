4d591eeb387fbe73b4cf2a640449a8a51d4a4565
@subhambiswas377flutternml <subhambiswas@gytworkz.com>
2021-12-11 14:34:48 +0530

    first commit

---
+++ lib/screens/homepage.dart
@@ -0,0 +1,18 @@
+import 'package:iot_log/utils/barrel.dart';
+
+
+
+class HomePage extends StatelessWidget
+{
+  const HomePage({Key? key}):super(key: key);
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(),
+      ),
+    );
+  }
+}
